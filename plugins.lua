9b1a14949a279a28d90f33760c66767320764359
Ian Reid <ian.young.reid@gmail.com>
2023-10-17 08:20:35 -0600

    Initial Commit

---
+++ plugins.lua
@@ -0,0 +1,81 @@
+local plugins = {
+  -- {
+  --  "iamcco/markdown-preview.nvim"
+  -- },
+  {
+    "rcarriga/nvim-dap-ui",
+    event = "VeryLazy",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function ()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "mfussenegger/nvim-dap"
+    },
+    opts = {
+      handlers = {}
+    }
+  },
+  {
+    "mfussenegger/nvim-dap",
+    config = function (_, _)
+      -- require("core.utils").load_mappings("dap")
+      vim.keymap.set('n', '<leader>do', '<cmd>lua require"dap".step_over()<CR>')
+      vim.keymap.set('n', '<leader>di', '<cmd>lua require"dap".step_into()<CR>')
+      vim.keymap.set('n', '<leader>dt', '<cmd>lua require"dap".step_out()<CR>')
+      vim.keymap.set('n', '<leader>dr', '<cmd>lua require"dap".continue()<CR>')
+      vim.keymap.set('n', '<leader>db', '<cmd>lua require"dap".toggle_breakpoint()<CR>')
+      vim.keymap.set('n', '<leader>dc', '<cmd>lua require"dap".set_breakpoint(vim.fn.input("Breakpoint condition: "))<CR>')
+      vim.keymap.set('n', '<leader>du', '<cmd>lua require"dapui".toggle()<CR>')
+    end
+  },
+  {
+    "mfussenegger/nvim-dap-python",
+    ft = "python",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "rcarriga/nvim-dap-ui"
+    },
+    config = function (_, opts)
+      local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python" -- To use a virtual environement.
+      require("dap-python").setup(path)
+      require("core.utils").load_mappings("dap_python")
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "clangd",
+        "clang-format",
+        "codelldb",
+        "debugpy",
+        "pyright",
+      }
+    }
+  }
+}
+return plugins
